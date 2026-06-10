9b3e39b61c1243373ec474977e964168b5c4a542
AlexSkilled <olegthander@yandex.ru>
2026-06-10 10:11:48 +0300

    [master] version upgrade

--- internal/repository/pg/queries/users.sql
+++ internal/repository/pg/queries/users.sql
@@ -2,7 +2,7 @@
 INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email, password_hash, created_at, updated_at;
 
 -- name: GetUserByID :one
-SELECT id, email, password_hash, created_at, updated_at FROM users WHERE id = $1;
+SELECT id, email, username, password_hash, created_at, updated_at FROM users WHERE id = $1;
 
 -- name: GetUserByEmail :one
 SELECT id, email, password_hash, created_at, updated_at FROM users WHERE email = $1;
