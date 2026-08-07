d986368d66dd597eefdb4cff094eb856a2c1d7d2
test <test@test.com>
2026-08-07 20:52:57 +0300

    [dependencies] logger

--- migrations/embed.go
+++ migrations/embed.go
@@ -3,8 +3,10 @@ package migrations
 import (
 	"database/sql"
 	"embed"
+	"fmt"
 
 	"github.com/pressly/goose/v3"
+	"github.com/rs/zerolog/log"
 	"go.redsock.ru/rerrors"
 )
 
@@ -15,7 +17,7 @@ var FS embed.FS
 // against conn. Safe to call on every startup — goose tracks applied
 // versions in the goose_db_version table and is a no-op once up to date.
 func ApplyMigration(conn *sql.DB) error {
-	goose.SetLogger(goose.NopLogger())
+	goose.SetLogger(_logger{})
 
 	err := goose.SetDialect("postgres")
 	if err != nil {
@@ -30,4 +32,19 @@ func ApplyMigration(conn *sql.DB) error {
 	}
 
 	return nil
+}
+
+type _logger struct {
+}
+
+func (l _logger) Fatalf(format string, v ...interface{}) {
+	log.Fatal().
+		Str("msg", fmt.Sprintf(format, v...)).
+		Msg("Fatal")
+}
+
+func (l _logger) Printf(format string, v ...interface{}) {
+	log.Info().
+		Str("msg", fmt.Sprintf(format, v...)).
+		Msg("Print")
 }
